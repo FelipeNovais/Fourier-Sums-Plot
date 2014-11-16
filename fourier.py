78bb7383c73117b452447ce740c4a7547336f385
Felipe Novais <felipenovais42@gmail.com>
2014-11-16 03:06:27 -0200

    V0.01

---
+++ fourier.py
@@ -0,0 +1,49 @@
+########################FOURIER PARTIAL SUMS########################
+#			-Created by Felipe Novais & Estela Mello-
+#		-Dedicated to Renato Cantao great guy and teacher-
+#
+#		How to: call the fourier.py with summation n-index
+#		example:
+#				$python fourier.py 5
+#				will result the first 5 terms
+####################################################################
+
+#IMPORTS
+import sys
+import matplotlib.pyplot as plt
+from numpy import pi, sin, cos, arange
+
+#VARIABLES
+#Fourier partial sums
+x = arange(-2 * pi, 2 * pi, 0.0001)
+y = (0.5)
+#Step function f(x)
+y2 = [1, 0, 1, 0, 1]
+x2 = [-2 * pi, -pi, 0, pi, 2 * pi]
+y3 = [0, 1, 0, 1, 0]
+
+#DEFINING THE PLOT AREA
+fig = plt.figure()
+ax  = fig.add_subplot(111)
+x_label = [r"$-\frac{\pi}{2}$", r"${\pi}$", r"$0$", r"${\pi}$",   r"$+\frac{\pi}{2}$"]
+ax.set_xticks([-2*pi, -pi, 0, pi, 2*pi])
+ax.set_xticklabels(x_label, fontsize=20)
+
+#PROCESSING THE SUMMATION
+if len(sys.argv) < 2:
+	j = 2
+else:
+	j = int(sys.argv[1]) + 1
+for i in range(1, j):
+	if(i % 2):
+		y += (2/(pi*i))*sin(i*x)
+
+#PLOTING
+ax.plot(x, y) #Fourier terms
+ax.step([-2 * pi, -pi],[1, 1],'r', [-pi, 0],[0, 0],'r', [0, pi],[1, 1],'r', [pi, 2 * pi],[0, 0], 'r') #f(x)
+ax.scatter(x2, y2, s = 30, c = 'r') #f(x) left boundaries
+ax.scatter(x2, y3, s = 30, c = 'w', edgecolors ='r') #f(x) right boundaries
+
+#SHOWING
+plt.grid()
+plt.show()
